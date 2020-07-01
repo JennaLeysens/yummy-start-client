71e4e8d862f70ceb6f68c00c702907f7cf35171f
JennaLeysens <jennalicowley@gmail.com>
2020-07-01 09:57:58 +0200

    added some test styling to recipe cards (with css)

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchRecipes } from "../store/recipes/actions";
 import { selectRecipes } from "../store/recipes/selectors";
+import "./Recipes.css";
 
 export default function Recipes() {
   const dispatch = useDispatch();
@@ -15,14 +16,18 @@ export default function Recipes() {
   return (
     <div>
       <h1>Recipes</h1>
-      {recipes.map((recipe, i) => {
-        return (
-          <div>
-            {recipe.title}{" "}
-            <img key={i} alt="recipe" height="300px" src={recipe.imageURL} />
-          </div>
-        );
-      })}
+      <div className="container">
+        {recipes.map((recipe, i) => {
+          return (
+            <div className="recipe">
+              <img key={i} alt="recipe" height="250px" src={recipe.imageURL} />{" "}
+              <div className="overlay">
+                <strong>{recipe.title}</strong>
+              </div>
+            </div>
+          );
+        })}{" "}
+      </div>
     </div>
   );
 }
