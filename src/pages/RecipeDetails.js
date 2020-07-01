3b1d09e9c66072a303e91250abbf74b4a672b819
JennaLeysens <jennalicowley@gmail.com>
2020-07-01 12:07:48 +0200

    fetch one recipe success

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import fetchOneRecipe from "../store/RecipeDetails/actions";
+import { fetchOneRecipe } from "../store/RecipeDetails/actions";
+import { selectRecipe } from "../store/RecipeDetails/selectors";
 
 export default function RecipeDetails() {
-  const id = useParams();
+  const { id } = useParams();
   const dispatch = useDispatch();
+  const recipe = useSelector(selectRecipe);
+  console.log("one recipe", recipe);
 
   useEffect(() => {
     dispatch(fetchOneRecipe(id));
