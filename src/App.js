a2a3d40ea714e15c1194459e25d737e6244d3611
JennaLeysens <jennalicowley@gmail.com>
2020-07-01 11:37:47 +0200

    created RecipeDetails route in App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Recipes from "./pages/Recipes";
+import RecipeDetails from "./pages/RecipeDetails";
 
 function App() {
   return (
     <div>
       <Switch>
+        <Route path="/recipes/:id" component={RecipeDetails} />
         <Route path="/" component={Recipes} />
       </Switch>
     </div>
