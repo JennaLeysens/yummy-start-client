723fd18a42b3bcafb23c05d80581cddea3d8e561
JennaLeysens <jennalicowley@gmail.com>
2020-07-01 11:53:28 +0200

    created one recipe selector

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,6 +1,15 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import fetchOneRecipe from "../store/RecipeDetails/actions";
 
 export default function RecipeDetails() {
+  const id = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchOneRecipe(id));
+  }, [dispatch, id]);
+
   return <div></div>;
 }
