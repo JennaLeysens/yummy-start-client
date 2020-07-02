d50d88ba7c83a7dca69663f3805669277827998d
JennaLeysens <jennalicowley@gmail.com>
2020-07-02 17:03:12 +0200

    add recipe action is working - must still add auth

--- src/pages/AddRecipe.js
+++ src/pages/AddRecipe.js
@@ -1,18 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addRecipe } from "../store/User/actions";
 
 export default function AddRecipe() {
+  const [title, setTitle] = useState();
+  const [imageURL, setImageURL] = useState();
+  const [description, setDescription] = useState();
+  const [ingredients, setIngredients] = useState();
+  const [method, setMethod] = useState();
+  const [cookingTime, setCookingTime] = useState();
+  const [submitted, setSubmitted] = useState(false);
+  const dispatch = useDispatch();
+
+  function submitForm(e) {
+    e.preventDefault();
+    setSubmitted(true);
+    dispatch(
+      addRecipe(title, imageURL, description, ingredients, method, cookingTime)
+    );
+    console.log(
+      "new recipe",
+      title,
+      imageURL,
+      description,
+      ingredients,
+      method,
+      cookingTime
+    );
+  }
+
   return (
     <div>
       <h1>Add your recipe</h1>
       <form>
-        Title<input></input>
-        Image URL<input></input>
-        Description<textarea></textarea>
-        Ingredients<input></input>
-        Method<textarea></textarea>
-        Cooking time<input></input>
+        Title
+        <input value={title} onChange={(e) => setTitle(e.target.value)}></input>
+        Image URL
+        <input
+          value={imageURL}
+          onChange={(e) => setImageURL(e.target.value)}
+        ></input>
+        Description
+        <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+        Ingredients
+        <input
+          value={ingredients}
+          onChange={(e) => setIngredients(e.target.value)}
+        ></input>
+        Method
+        <textarea
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        ></textarea>
+        Cooking time
+        <input
+          value={cookingTime}
+          onChange={(e) => setCookingTime(e.target.value)}
+        ></input>
       </form>
-      <button>Post recipe</button>
+      <button onClick={submitForm}>Post recipe</button>
     </div>
   );
 }
