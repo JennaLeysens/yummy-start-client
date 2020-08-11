528d9ae967a7f011042a0e900b7a9d72390f7397
JennaLeysens <jennalicowley@gmail.com>
2020-08-11 18:16:23 +0200

    fetching images from Cloudinary

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Recipes from "./pages/Recipes";
@@ -13,6 +13,7 @@ import UserProfile from "./pages/UserProfile";
 import OurFoodosophy from "./pages/OurFoodosophy";
 import Loading from "./components/Loading";
 import { selectAppLoading } from "./store/Appstate/selectors";
+import { CloudinaryContext } from "cloudinary-react";
 
 function App() {
   const dispatch = useDispatch();
@@ -23,20 +24,22 @@ function App() {
   }, [dispatch]);
 
   return (
-    <div className="App" style={{ display: "block" }}>
-      <NavBar></NavBar>
-      {isLoading ? <Loading /> : null}
-      <Switch>
-        <Route path="/ourfoodosophy" component={OurFoodosophy} />
-        <Route path="/addrecipe" component={AddRecipe} />
-        <Route path="/recipes/:id" component={RecipeDetails} />
-        <Route exact path="/" component={Recipes} />
-        <Route path="/myprofile" component={UserProfile} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-      </Switch>
-      <div className="footer"></div>
-    </div>
+    <CloudinaryContext cloudName="yummystart">
+      <div className="App" style={{ display: "block" }}>
+        <NavBar></NavBar>
+        {isLoading ? <Loading /> : null}
+        <Switch>
+          <Route path="/ourfoodosophy" component={OurFoodosophy} />
+          <Route path="/addrecipe" component={AddRecipe} />
+          <Route path="/recipes/:id" component={RecipeDetails} />
+          <Route exact path="/" component={Recipes} />
+          <Route path="/myprofile" component={UserProfile} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+        </Switch>
+        <div className="footer"></div>
+      </div>
+    </CloudinaryContext>
   );
 }
 
